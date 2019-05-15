8e8c03aa4e146533fed619cda98c4dc2e1c97393
Richard Smith <mercurial@fantastic.me.uk>
2019-05-15 14:19:58 +0100

    more advanced stuff

---
+++ 3_advanced/26_classes.py
@@ -0,0 +1,26 @@
+# You've already been using class types, e.g. Rect and Actor
+# But these classes do not include vx and vy variables, so let's
+# create our own class, Sprite, that is the same as Actor but with
+# these variables included
+
+WIDTH = 500
+HEIGHT = 500
+
+class Sprite(Actor):
+    vx = 1
+    vy = 1
+
+ball = Sprite('alien')
+
+def draw():
+    screen.clear()
+    ball.draw()
+
+def update():
+    ball.x += ball.vx
+    ball.y += ball.vy
+    if ball.right > WIDTH or ball.left < 0:
+        ball.vx = -ball.vx
+    if ball.bottom > HEIGHT or ball.top < 0:
+        ball.vy = -ball.vy
+
