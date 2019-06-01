28ff0d68b714baaf9cf2bcc64de4e1f57fe387ea
electronstudio <misc@electronstudio.co.uk>
2019-06-01 01:57:08 +0100

    tom fixes

--- 0_text_quizes/06_loop.py
+++ 0_text_quizes/06_loop.py
@@ -3,13 +3,13 @@ import random
 n = random.randint(0, 10)
 
 while True:
-    print("I am thinking of a number, can you guess it?")
+    print("I am thinking of a number, can you guess what it is?")
     g = int(input())
     if g == n:
         break
     else:
-        print("wrong")
-print("correct!")
+        print("Wrong")
+print("Correct!")
 
 """ TODO:
     give a hint to the player when they are wrong
