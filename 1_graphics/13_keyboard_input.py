579413077cc1cb6ccc44657ec95631823866820d
electronstudio <misc@fantastic.me.uk>
2019-09-06 16:33:22 +0100

    chase stuff

--- 1_graphics/13_keyboard_input.py
+++ 1_graphics/13_keyboard_input.py
@@ -8,9 +8,9 @@ def draw():
 
 
 def update():
-    if (keyboard.right):
+    if keyboard.right:
         alien.x = alien.x + 2
-    elif (keyboard.left):
+    elif keyboard.left:
         alien.x = alien.x - 2
 
 
